c083ddb56c52ca11a79648004af6c51690207dd3
Thiago Magano <thiagomagano1993@outlook.com>
2025-02-05 19:00:40 -0300

    Conseumindo de json e saindo dados com JSON

--- main.go
+++ main.go
@@ -1,58 +1,103 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"os"
 	"sort"
+	"time"
 )
 
 type Jogador struct {
-	Nome  string
-	Nivel int
+	Nome  string `json:"nome"`
+	Nivel int    `json:"nivel"`
 }
 
-type Time struct {
-	Jogadores []Jogador
+func carregarJogadoresJSON(caminho string) ([]Jogador, error) {
+	file, err := os.Open(caminho)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var jogadores []Jogador
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&jogadores); err != nil {
+		return nil, err
+	}
+
+	return jogadores, nil
 }
 
-var jogadores []Jogador = []Jogador{
-	{Nome: "Thiago", Nivel: 4},
-	{Nome: "Paulo", Nivel: 5},
-	{Nome: "Marcel", Nivel: 3},
-	{Nome: "Thales", Nivel: 5},
-	{Nome: "Brunão", Nivel: 1},
-	{Nome: "Juliano", Nivel: 3},
-	{Nome: "Claudinho", Nivel: 4},
-	{Nome: "Serginho", Nivel: 1},
-	{Nome: "Waldeka", Nivel: 2},
-	{Nome: "Higor", Nivel: 2},
+func salvarTimesEmArquivo(time1, time2 []Jogador, nivelTime1, nivelTime2 int) error {
+	timestamp := time.Now().Format("20060102_150405")
+	caminho := fmt.Sprintf("times/times_%s.json", timestamp)
+
+	file, err := os.Create(caminho)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	dados := map[string]interface{}{
+		"time1": map[string]interface{}{
+			"jogadores":       time1,
+			"total_jogadores": len(time1),
+			"nivel_total":     nivelTime1,
+		},
+		"time2": map[string]interface{}{
+			"jogadores":       time2,
+			"total_jogadores": len(time2),
+			"nivel_total":     nivelTime2,
+		},
+	}
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(dados); err != nil {
+		return err
+	}
+
+	fmt.Printf("Times salvos em '%s'\n", caminho)
+	return nil
 }
 
-func OrdenarTime(jogadores []Jogador) []Jogador {
-
-	sort.SliceStable(jogadores, func(i, j int) bool {
+func balancearTimes(jogadores []Jogador) ([]Jogador, []Jogador, int, int) {
+	sort.Slice(jogadores, func(i, j int) bool {
 		return jogadores[i].Nivel > jogadores[j].Nivel
 	})
 
-	return jogadores
-}
+	var time1, time2 []Jogador
+	var nivelTime1, nivelTime2 int
 
-func SepararTimes(jogadores []Jogador) (timeA, timeB Time) {
-	jgOrdenados := OrdenarTime(jogadores)
-
-	a := Time{}
-	b := Time{}
-
-	for i, jogador := range jgOrdenados {
-		if i%2 == 0 {
-			a.Jogadores = append(a.Jogadores, jogador)
+	for _, jogador := range jogadores {
+		if (nivelTime1 < nivelTime2) || (nivelTime1 == nivelTime2 && len(time1) <= len(time2)) {
+			time1 = append(time1, jogador)
+			nivelTime1 += jogador.Nivel
 		} else {
-			b.Jogadores = append(b.Jogadores, jogador)
+			time2 = append(time2, jogador)
+			nivelTime2 += jogador.Nivel
 		}
 	}
 
-	return a, b
+	return time1, time2, nivelTime1, nivelTime2
 }
 
 func main() {
-	fmt.Println("Hello Separador de Times")
+	arquivo := flag.String("arquivo", "exemplo_jogadores.json", "Caminho do arquivo JSON com os jogadores")
+	flag.Parse()
+
+	jogadores, err := carregarJogadoresJSON(*arquivo)
+	if err != nil {
+		fmt.Println("Erro ao carregar jogadores:", err)
+		return
+	}
+
+	time1, time2, nivelTime1, nivelTime2 := balancearTimes(jogadores)
+
+	err = salvarTimesEmArquivo(time1, time2, nivelTime1, nivelTime2)
+	if err != nil {
+		fmt.Println("Erro ao salvar times:", err)
+	}
 }
