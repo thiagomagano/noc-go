90ecec2fa9622b3f85f50808a33d984fa7280165
Thiago Magano <thiagomagano1993@outlook.com>
2025-02-05 17:38:16 -0300

    Ordenando e Separando times

--- main.go
+++ main.go
@@ -1,11 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
-func Hello(msg string) string {
-	return msg
+type Jogador struct {
+	Nome  string
+	Nivel int
+}
+
+type Time struct {
+	Jogadores []Jogador
+}
+
+var jogadores []Jogador = []Jogador{
+	{Nome: "Thiago", Nivel: 4},
+	{Nome: "Paulo", Nivel: 5},
+	{Nome: "Marcel", Nivel: 3},
+	{Nome: "Thales", Nivel: 5},
+	{Nome: "Brunão", Nivel: 1},
+	{Nome: "Juliano", Nivel: 3},
+	{Nome: "Claudinho", Nivel: 4},
+	{Nome: "Serginho", Nivel: 1},
+	{Nome: "Waldeka", Nivel: 2},
+	{Nome: "Higor", Nivel: 2},
+}
+
+func OrdenarTime(jogadores []Jogador) []Jogador {
+
+	sort.SliceStable(jogadores, func(i, j int) bool {
+		return jogadores[i].Nivel > jogadores[j].Nivel
+	})
+
+	return jogadores
+}
+
+func SepararTimes(jogadores []Jogador) (timeA, timeB Time) {
+	jgOrdenados := OrdenarTime(jogadores)
+
+	a := Time{}
+	b := Time{}
+
+	for i, jogador := range jgOrdenados {
+		if i%2 == 0 {
+			a.Jogadores = append(a.Jogadores, jogador)
+		} else {
+			b.Jogadores = append(b.Jogadores, jogador)
+		}
+	}
+
+	return a, b
 }
 
 func main() {
-	fmt.Println(Hello("Hello Separador de Times"))
+	fmt.Println("Hello Separador de Times")
 }
--- main_test.go
+++ main_test.go
@@ -1,12 +1,58 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestHello(t *testing.T) {
-	got := Hello("Hello Separador de Times")
-	want := "Hello Separador de Times"
-
-	if want != got {
-		t.Errorf("want %s got %s", want, got)
+func TestOrdenarTime(t *testing.T) {
+	want := []Jogador{
+		{Nome: "Paulo", Nivel: 5},
+		{Nome: "Thales", Nivel: 5},
+		{Nome: "Thiago", Nivel: 4},
+		{Nome: "Claudinho", Nivel: 4},
+		{Nome: "Marcel", Nivel: 3},
+		{Nome: "Juliano", Nivel: 3},
+		{Nome: "Waldeka", Nivel: 2},
+		{Nome: "Higor", Nivel: 2},
+		{Nome: "Brunão", Nivel: 1},
+		{Nome: "Serginho", Nivel: 1},
 	}
+
+	got := OrdenarTime(jogadores)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("want %v got %v", want, got)
+	}
+}
+
+func TestSepararTimes(t *testing.T) {
+	wantTime1 := Time{
+		[]Jogador{
+			{Nome: "Paulo", Nivel: 5},
+			{Nome: "Thiago", Nivel: 4},
+			{Nome: "Marcel", Nivel: 3},
+			{Nome: "Waldeka", Nivel: 2},
+			{Nome: "Brunão", Nivel: 1},
+		},
+	}
+	wantTime2 := Time{
+		[]Jogador{
+			{Nome: "Thales", Nivel: 5},
+			{Nome: "Claudinho", Nivel: 4},
+			{Nome: "Juliano", Nivel: 3},
+			{Nome: "Higor", Nivel: 2},
+			{Nome: "Serginho", Nivel: 1},
+		},
+	}
+	gotTime1, gotTime2 := SepararTimes(jogadores)
+
+	if !reflect.DeepEqual(wantTime1, gotTime1) {
+		t.Errorf("want time 1 %v got time1 %v", wantTime1, gotTime1)
+	}
+
+	if !reflect.DeepEqual(wantTime2, gotTime2) {
+		t.Errorf("want time 1 %v got time1 %v", wantTime2, gotTime2)
+	}
+
 }
