11355d449bab36778cce58bac296b2c4ac09f5c2
Thiago Magano <thiagomagano1993@outlook.com>
2025-02-25 14:36:59 -0300

    flag output format

--- main.go
+++ main.go
@@ -30,33 +30,56 @@ func carregarJogadoresJSON(caminho string) ([]Jogador, error) {
 	return jogadores, nil
 }
 
-func salvarTimesEmArquivo(time1, time2 []Jogador, nivelTime1, nivelTime2 int) error {
+func salvarTimesEmArquivo(time1, time2 []Jogador, nivelTime1, nivelTime2 int, formato string) error {
 	timestamp := time.Now().Format("20060102_150405")
-	caminho := fmt.Sprintf("times/times_%s.json", timestamp)
+	var caminho string
 
-	file, err := os.Create(caminho)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
+	if formato == "md" {
+		caminho = fmt.Sprintf("times/times_%s.md", timestamp)
+		file, err := os.Create(caminho)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
 
-	dados := map[string]interface{}{
-		"time1": map[string]interface{}{
-			"jogadores":       time1,
-			"total_jogadores": len(time1),
-			"nivel_total":     nivelTime1,
-		},
-		"time2": map[string]interface{}{
-			"jogadores":       time2,
-			"total_jogadores": len(time2),
-			"nivel_total":     nivelTime2,
-		},
-	}
+		fmt.Fprintf(file, "# *Atenção aos times!*\n\n\n")
 
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-	if err := encoder.Encode(dados); err != nil {
-		return err
+		fmt.Fprintf(file, "*Time Branco(%d)*:\n", len(time1))
+		for _, jogador := range time1 {
+			fmt.Fprintf(file, "- %s\n", jogador.Nome)
+		}
+
+		fmt.Fprintf(file, "\n*Time Preto (%d)*:\n", len(time2))
+		for _, jogador := range time2 {
+			fmt.Fprintf(file, "- %s\n", jogador.Nome)
+		}
+
+	} else {
+		caminho = fmt.Sprintf("times/times_%s.json", timestamp)
+		file, err := os.Create(caminho)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		dados := map[string]interface{}{
+			"time1": map[string]interface{}{
+				"jogadores":       time1,
+				"total_jogadores": len(time1),
+				"nivel_total":     nivelTime1,
+			},
+			"time2": map[string]interface{}{
+				"jogadores":       time2,
+				"total_jogadores": len(time2),
+				"nivel_total":     nivelTime2,
+			},
+		}
+
+		encoder := json.NewEncoder(file)
+		encoder.SetIndent("", "  ")
+		if err := encoder.Encode(dados); err != nil {
+			return err
+		}
 	}
 
 	fmt.Printf("Times salvos em '%s'\n", caminho)
@@ -86,6 +109,7 @@ func balancearTimes(jogadores []Jogador) ([]Jogador, []Jogador, int, int) {
 
 func main() {
 	arquivo := flag.String("arquivo", "exemplo_jogadores.json", "Caminho do arquivo JSON com os jogadores")
+	formato := flag.String("formato", "json", "Formato de saída (txt ou json)")
 	flag.Parse()
 
 	jogadores, err := carregarJogadoresJSON(*arquivo)
@@ -96,7 +120,7 @@ func main() {
 
 	time1, time2, nivelTime1, nivelTime2 := balancearTimes(jogadores)
 
-	err = salvarTimesEmArquivo(time1, time2, nivelTime1, nivelTime2)
+	err = salvarTimesEmArquivo(time1, time2, nivelTime1, nivelTime2, *formato)
 	if err != nil {
 		fmt.Println("Erro ao salvar times:", err)
 	}
